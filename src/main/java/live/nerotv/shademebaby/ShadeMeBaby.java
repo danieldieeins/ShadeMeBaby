1bcf4b74fa30bb0ad585c857ee9c4de79a3ae495
danieldieeins <daniel.niesmann@outlook.com>
2023-09-22 12:29:35 +0200

    1.1.0

--- src/main/java/live/nerotv/shademebaby/ShadeMeBaby.java
+++ src/main/java/live/nerotv/shademebaby/ShadeMeBaby.java
@@ -6,15 +6,6 @@ public class ShadeMeBaby {
 
     private static Logger logger = new Logger("ShadeMeBaby");
 
-    /*public static void main(String[] testArgs) throws IOException {
-        logger = new Logger("ShadeMeBaby");
-        logger.setDebugEnabled(true);
-        NFrame frame = new NFrame();
-        frame.setIconFromResources("icosn.png");
-        frame.setSize(new Dimension(50,50));
-        frame.setLocationRelativeTo(null); frame.setVisible(true);
-    }*/
-
     public static Logger getLogger() {
         return logger;
     }
