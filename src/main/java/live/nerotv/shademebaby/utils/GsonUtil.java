1dad2452ef6e741cd08920529d276c531b3cd236
nerotvlive <daniel.niesmann@outlook.com>
2024-02-08 13:21:28 +0100

    Add GsonUtil

---
+++ src/main/java/live/nerotv/shademebaby/utils/GsonUtil.java
@@ -0,0 +1,31 @@
+package live.nerotv.shademebaby.utils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GsonUtil {
+
+    private static JsonObject getObject(String url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                return JsonParser.parseString(response.toString()).getAsJsonObject();
+            }
+            connection.disconnect();
+        } catch (Exception ignore) {}
+        return null;
+    }
+}
